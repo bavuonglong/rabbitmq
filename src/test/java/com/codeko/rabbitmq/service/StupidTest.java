cd7c118fe5556ed9653e74e46c628dae316a5de3
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-06-23 21:45:44 +0700

    Add CI with circle ci

---
+++ src/test/java/com/codeko/rabbitmq/service/StupidTest.java
@@ -0,0 +1,12 @@
+package com.codeko.rabbitmq.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StupidTest {
+
+    @Test
+    public void stupidTest() {
+        Assert.assertEquals(2, 1 + 1);
+    }
+}
